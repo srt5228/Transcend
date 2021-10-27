901837b26115d76d85a5fc37282d00694dc29319
Scottie Thompson <59934685+srt5228@users.noreply.github.com>
2021-10-26 21:32:19 -0400

    Setting up react router and App/Dashboard components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import '../App.css';
+import Dashboard from "./Dashboard";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Router>
+          <Route path="/" exact>
+            <Dashboard/>
+          </Route>
+        </Router>
+      </div>
   );
 }
 
---
+++ client/src/Dashboard.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+
+function Dashboard() {
+
+    return (
+        <>
+            <h1>HOME PAGE</h1>
+        </>
+    );
+};
+
+export default Dashboard;
