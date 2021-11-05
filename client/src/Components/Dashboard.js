e51bc94cebb98e59ed1c907d1e583a83149e2adb
Scottie Thompson <scottiecodes@gmail.com>
2021-11-04 21:05:11 -0400

    Edited links

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -8,13 +8,13 @@ function Dashboard() {
         <>
             <h1>Transcend Home Page</h1>
             <p>Welcome to the Transcend Habit Tracker Application Database!</p>
-            <Link to="/users"><b>Users Page</b> <br></br> View and manage users</Link>
-            <Link to="/habits"><b>Habits Page</b> <br></br> View and manage habits</Link>
-            <Link to="/focustimer"><b>Focus Timers Page</b> <br></br> View and manage focus timers</Link>
-            <Link to="/journalentries"><b>Journal Entries Page</b> <br></br> View and manage journal entries</Link>
-            <Link to="/logeventtypes"><b>Log Event Types Page</b> <br></br> View and manage user log event types</Link>
-            <Link to="/userhabits"><b>User Habits Page</b> <br></br> View and manage the habits of users</Link>
-            <Link to="/userlogs"><b>User Logs Page</b> <br></br> View and manage the log events of users</Link>
+            <Link to="/users"><b>Users Table</b> <br></br></Link>
+            <Link to="/habits"><b>Habits Table</b> <br></br></Link>
+            <Link to="/focustimer"><b>Focus Timers Table</b> <br></br></Link>
+            <Link to="/journalentries"><b>Journal Entries Table</b> <br></br></Link>
+            <Link to="/logeventtypes"><b>Log Event Types Table</b> <br></br></Link>
+            <Link to="/userhabits"><b>User Habits Table</b> <br></br></Link>
+            <Link to="/userlogs"><b>User Logs Table</b> <br></br></Link>
 
 
         </>
